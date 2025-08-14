a8a71ad49518d7b5551676d73026df3d28bccbe5
funmibi <abiodunfunmibi17@gmail.com>
2025-08-13 22:05:26 -1200

    submit button updates

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+ 
+ // navbar toggle 
  const bar = document.querySelector(".toggle");
  const menu = document.querySelector(".navbar-menu");
 
@@ -6,6 +8,7 @@
         menu.classList.toggle('active')
     }
 
+    // dropdown content 
  const dropbtn = document.querySelector(".dropbtn");
  const dropcontent = document.querySelector(".dropcontent");
 
@@ -14,5 +17,18 @@
         dropcontent.classList.toggle('click');
     }
 
+    // submit news letter
+const email = document.getElementById("email");
+const submitBtn = document.getElementById("submit-btn");
+let output = document.querySelector(".email-output");
 
- 
+submitBtn.onclick = function(){
+    let emailEntry = email.value;
+
+    if(emailEntry === ""){
+        output.textContent = "PLEASE INPUT YOUR EMAIL!"
+    }
+    else{
+        alert(`YOU HAVE SUCCESSFULLY SIGNED ${emailEntry}`);
+    }
+}
